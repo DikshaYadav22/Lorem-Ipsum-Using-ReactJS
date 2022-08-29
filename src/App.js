70461cd449d622998a47faaef1611f59b9a25ccf
bob <diksha.yadav022@gmail.com>
2022-08-29 14:14:56 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React, {useState} from 'react';
+import text from './data';
+import {Card, CardBody} from 'reactstrap';
+
+const App = () => {
+  const[inputHandler, setInputHandler] = useState(0);
+  const [data, setData] = useState(text);
+  let [items, setItems] = useState();
+  const handleSubmit = (e) => 
+  {
+    e.preventDefault();
+  }
+
+  const createIpsum = () => 
+  {
+    let items = [...data];
+    const filteredData = data.slice(0, inputHandler);
+      setItems(filteredData);
+  }
+
+  return (
+    <div className="App">
+        <h3 className="mt-2">TIRED OF BORING LOREM IPSUM?</h3>
+        <div>
+            Paragraphs: 
+            <form onSubmit={(e)=>handleSubmit(e)}>
+              <input type="number" name="lorem"
+                value={inputHandler}
+                onChange={(e)=>setInputHandler(e.target.value)}
+                
+                
+              />
+            <button onClick={()=>createIpsum()}>Generate</button>
+
+            </form>
+            
+        </div>
+
+        <Card>
+            <CardBody>
+                {items} + "<br></br>"
+            </CardBody>
+        </Card>
+    </div>
+  );
+}
+
+export default App;
